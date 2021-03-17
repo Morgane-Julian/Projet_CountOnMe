99202b0075fd43cb6776f8f11e1daad15c295470
Morgane <morgane.julian@symbioz.io>
2021-03-17 17:55:18 +0100

    - Contraintes OK - Ajout boutons * et / dans le storyboard - Répartitions des fichiers MVC

---
+++ CountOnMe/Model/CalcManager.swift
@@ -0,0 +1,12 @@
+//
+//  CalcManager.swift
+//  CountOnMe
+//
+//  Created by Symbioz on 17/03/2021.
+//  Copyright © 2021 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+class CalcManager {
+
+}
